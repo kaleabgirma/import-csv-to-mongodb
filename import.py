2462f07a45e6e5b99e232a6df79cb43d63944256
Kaleab Girma <ethiocodders@gmail.com>
2022-04-14 10:30:39 +0300

    Adding the import.py file

---
+++ import.py
@@ -0,0 +1,31 @@
+import pymongo
+import pandas as pd
+import json
+
+connection_string = "mongodb://localhost:27017"
+csv_file_path = "talents-data-toindex.csv"
+database_name = "talents_import"
+collection_name = "collected_talents"
+
+try:
+  client = pymongo.MongoClient(connection_string)
+except Exception as e:
+    print("An Error occured while connectig the Database \n"+e)
+
+try:
+    df = pd.read_csv(csv_file_path)
+except Exception as e:
+    print("An Error occured while reading CSV file \n"+e)
+try:
+    data = df.to_dict(orient = "records")
+except Exception as e:
+    print("An Error occured while converting the data to dictionary \n"+e)
+try:
+    db = client[database_name]
+except Exception as e:
+    print("An Error occured on database operation \n"+e)
+try:
+    db[collection_name].insert_many(data)
+    print("Data inserted Successfuly")
+except Exception as e:
+    print("An Error occured on data insertion operation \n"+e)
